a9b44a6d520972690465b5fe6c2b7f1c6ce32c57
UGO COELHO <2024102070@academicougb.com.br>
2026-05-23 16:28:58 -0300

    Adicionar arquivos de projeto.

---
+++ BancoQuestoes/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using BancoQuestoes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BancoQuestoes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BancoQuestoes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BancoQuestoes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
