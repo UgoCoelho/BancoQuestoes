55b164a35636da4d3f7b0348b342c535044ca8b6
Gabrelsonn <gabrielseixas117@gmail.com>
2026-05-24 17:23:02 -0300

    Foi

---
+++ SQLQuery1.sql
@@ -0,0 +1,4 @@
+INSERT INTO Curso (Nome) VALUES ('Sistema de Informação');
+
+INSERT INTO Usuario (Matricula, Nome, Senha, Periodo, CursoId, Role)
+VALUES (99999999, 'Administrador', 'Admin@123', 1, 1, 1);
---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+
+
+INSERT INTO Usuario (Matricula, Nome, Senha, Periodo, CursoId, Role)
+VALUES (99999999, 'Administrador', 'Admin@123', 1, 1, 1);
