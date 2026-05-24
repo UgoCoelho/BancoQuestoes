99c4504b8b7eef0834a553f6f2b5089bbf75560f
Carlos Eduardo <eduardofilho03@hotmail.com>
2026-05-23 22:59:45 -0300

    Feat: Adicionado Model e Controller da Entidade Arquivo.

---
+++ BancoQuestoes/Models/Arquivo.cs
@@ -0,0 +1,22 @@
+﻿namespace BancoQuestoes.Models
+{
+    public class Arquivo
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Professor { get; set; }
+
+        public string Tag { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Curso CursoId { get; set; }
+
+        public Materia MateriaId { get; set; }
+
+        public TipoArquivo TipoArquivoId { get; set; }
+
+    }
+}
