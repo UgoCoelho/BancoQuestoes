ab2f1c82f7b19eb5074e291dc4ceb2209985324a
laurasferreiras <laurasferreira6@gmail.com>
2026-05-23 22:39:51 -0300

    Adição da View Períodos e e estilização da página

--- BancoQuestoes/Controllers/HomeController.cs
+++ BancoQuestoes/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace BancoQuestoes.Controllers
             return View();
         }
 
+        //fazendo testes
+        public IActionResult Periodos()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
