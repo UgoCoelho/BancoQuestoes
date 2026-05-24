1a63e645af658a5bcaf5e92c5a5a884e3665489b
Carlos Eduardo <eduardofilho03@hotmail.com>
2026-05-24 20:11:03 -0300

    Atualização de css dos formulários.

--- BancoQuestoes/Migrations/20260524211946_start.cs
+++ BancoQuestoes/Migrations/20260524211946_start.cs
@@ -5,7 +5,7 @@
 namespace BancoQuestoes.Migrations
 {
     /// <inheritdoc />
-    public partial class oitavioinitial : Migration
+    public partial class start : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
