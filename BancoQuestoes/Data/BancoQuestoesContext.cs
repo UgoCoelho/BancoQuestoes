8be30484defeab99845e7889d4f500052d556628
UGO COELHO <2024102070@academicougb.com.br>
2026-05-24 12:42:22 -0300

    exclusão de periodo

--- BancoQuestoes/Data/BancoQuestoesContext.cs
+++ BancoQuestoes/Data/BancoQuestoesContext.cs
@@ -15,7 +15,12 @@ namespace BancoQuestoes.Data
         public DbSet<TipoArquivo> TipoArquivo { get; set; }
         public DbSet<Curso> Curso { get; set; }
         public DbSet<Arquivo> Arquivo { get; set; }
+<<<<<<< HEAD
+        
+=======
         public DbSet<Periodo> Periodo { get; set; }
 
+>>>>>>> a0afb550f68335e6da8cc4494daf5d7b8d99b147
     }
 }
+
